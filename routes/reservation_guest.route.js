627cc6a1e3651485abe0e3c20284f7bf10a0b17b
AlexisFAP <alexispatinoagudelo@gmail.com>
2022-05-12 00:11:37 -0500

    reservation guest fixed

--- routes/reservation_guest.route.js
+++ routes/reservation_guest.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getItem, createItem } = require('../controllers/guest/reservation_guest.controller');
 const { validatorCreateItem, validatorGetItem } = require('../validators//guest/reservation_guest.validator');
 
-router.get('/:id', validatorGetItem, getItem);
+router.get('/:name', validatorGetItem, getItem);
 
 router.post('/', validatorCreateItem, createItem);
 
--- validators/guest/reservation_guest.validator.js
+++ validators/guest/reservation_guest.validator.js
@@ -4,9 +4,9 @@ const validateResults = require('../../utils/handleValidator');
 const validatorCreateItem = [
     check("guest_last_name").exists().notEmpty(),
     check("guest_id").exists().notEmpty().isLength(10),
-    check("confirmation_number").exists().notEmpty(),
+    check("confirm_number").exists().notEmpty(),
     check("hotel_id").exists().notEmpty(),
-    check("star_date").exists().notEmpty(),
+    check("start_date").exists().notEmpty(),
     check("end_date").exists().notEmpty(),
     check("room_number").exists().notEmpty(),
     (req, res, next) => {return validateResults(req, res, next)}
